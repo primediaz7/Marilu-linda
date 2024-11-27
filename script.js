b5f97ee5b0fcd90f6cfd4e9aa9073ee2aebcfabd
primediaz7 <pedronaldi07@gmail.com>
2024-11-27 09:56:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function mostrarMensagem() {
+    const mensagem = document.getElementById('mensagem');
+    mensagem.textContent = 'Você é a pessoa mais especial da minha vida! ❤️';
+    mensagem.style.display = 'block';
+}
